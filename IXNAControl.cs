55c746c2d4499cb2031733bd5951458d2e017a35
Ethan Moffat <ethanmoffat@hotmail.com>
2016-11-26 15:35:41 -0800

    Add IXNAControl interface with some possible properties/methods
    Used existing XNAControl class as a template for what the functionality should be

---
+++ IXNAControl.cs
@@ -0,0 +1,31 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace XNAControls
+{
+    public interface IXNAControl : IGameComponent, IDrawable, IUpdateable, IDisposable
+    {
+        Vector2 DrawPosition { get; }
+
+        Rectangle DrawArea { get; }
+
+        Rectangle DrawAreaWithParentOffset { get; }
+
+        IXNAControl ImmediateParent { get; }
+
+        IXNAControl TopParent { get; }
+
+        IReadOnlyList<IXNAControl> ChildControls { get; }
+
+        void AddControlToDefaultGame();
+
+        void AddControlToSpecificGame(Game game);
+
+        void SetParentControl(IXNAControl parent);
+    }
+}
