6b4fa9975df2319c645c6660e12aaf107ee242d9
Ethan Moffat <ethanmoffat@hotmail.com>
2016-11-26 15:17:22 -0800

    Add GameRepository to hold an instance of a game to be used for all XNAControls by default

---
+++ GameRepository.cs
@@ -0,0 +1,23 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using Microsoft.Xna.Framework;
+
+namespace XNAControls
+{
+    public class GameRepository
+    {
+        private Game Game { get; set; }
+
+        public static void SetGame(Game game)
+        {
+            Singleton<GameRepository>.Instance.Game = game;
+        }
+
+        public static Game GetGame()
+        {
+            return Singleton<GameRepository>.Instance.Game;
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,30 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System;
+using System.Collections.Generic;
+
+namespace XNAControls
+{
+    internal class Singleton<T> : Singleton where T : class, new()
+    {
+        public static T Instance
+        {
+            get
+            {
+                var typeKey = typeof(T);
+
+                if(!_typeMap.ContainsKey(typeKey) || _typeMap[typeKey] == null)
+                    _typeMap.Add(typeKey, new T());
+
+                return (T)_typeMap[typeKey];
+            }
+        }
+    }
+
+    internal class Singleton
+    {
+        protected static readonly Dictionary<Type, object> _typeMap = new Dictionary<Type, object>();
+    }
+}
