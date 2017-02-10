9aceae56ff4fdc490f990b2043be5c2889eabf77
Ethan Moffat <ethanmoffat@hotmail.com>
2017-02-10 12:40:11 -0800

    Add TestGameManager class to manage a game reference for tests

---
+++ XNAControls.Test/Helpers/TestGameManager.cs
@@ -0,0 +1,36 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2017
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XNAControls.Test.Helpers
+{
+    public class TestGameManager : IDisposable
+    {
+        public Game Game { get; }
+        public GraphicsDeviceManager GraphicsDeviceManager { get; }
+        
+        public TestGameManager()
+        {
+            Game = new Game();
+            GraphicsDeviceManager = new GraphicsDeviceManager(Game);
+
+            //remove the default graphics device service and inject our own
+            Game.Services.RemoveService(typeof(IGraphicsDeviceService));
+
+            var gds = GraphicsDeviceService.AddRef(Game.Window.Handle,
+                                                   Game.Window.ClientBounds.Width,
+                                                   Game.Window.ClientBounds.Height);
+            Game.Services.AddService(typeof(IGraphicsDeviceService), gds);
+        }
+
+        public void Dispose()
+        {
+            GraphicsDeviceManager.Dispose();
+            Game.Dispose();
+        }
+    }
+}
