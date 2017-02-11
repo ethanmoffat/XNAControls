8da5f7bc109f5e453bb6db15383662799740bc7b
Ethan Moffat <ethanmoffat@hotmail.com>
2017-02-11 10:18:21 -0800

    Add Keyboard and Mouse adapters

---
+++ Adapters/KeyboardAdapter.cs
@@ -0,0 +1,23 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2017
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using Microsoft.Xna.Framework.Input;
+
+namespace XNAControls.Adapters
+{
+    internal class KeyboardAdapter : IKeyboardAdapter
+    {
+        static KeyboardAdapter()
+        {
+            Singleton<IKeyboardAdapter, KeyboardAdapter>.Map();
+        }
+
+        public KeyboardState State => Keyboard.GetState();
+    }
+
+    internal interface IKeyboardAdapter
+    {
+        KeyboardState State { get; }
+    }
+}
---
+++ Adapters/MouseAdapter.cs
@@ -0,0 +1,23 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2017
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using Microsoft.Xna.Framework.Input;
+
+namespace XNAControls.Adapters
+{
+    internal class MouseAdapter : IMouseAdapter
+    {
+        static MouseAdapter()
+        {
+            Singleton<IMouseAdapter, MouseAdapter>.Map();
+        }
+
+        public MouseState State => Mouse.GetState();
+    }
+
+    internal interface IMouseAdapter
+    {
+        MouseState State { get; }
+    }
+}
