687a541c6ca318707ca53a52b8c2c072c9518ec7
Ethan Moffat <ethan@moffat.io>
2022-02-10 10:41:01 -0800

    Fix handling of TextInput event in game window to avoid MonoGame bug on Linux where event fires twice for control characters

--- XNAControls/KeyboardEvents.cs
+++ XNAControls/KeyboardEvents.cs
@@ -2,6 +2,7 @@
 //// This file is subject to the GPL v2 License
 //// For additional details, see the LICENSE file
 
+using System;
 using Microsoft.Xna.Framework;
 
 namespace XNAControls
@@ -12,14 +13,24 @@ namespace XNAControls
 
         private readonly GameWindow _window;
 
+        private DateTime _lastInputTime;
+
         public KeyboardEvents(GameWindow window)
         {
             _window = window;
             _window.TextInput += GameWindow_TextInput;
+            _lastInputTime = DateTime.Now;
         }
 
         private void GameWindow_TextInput(object sender, TextInputEventArgs e)
         {
+            // DateTime has a precision of about 15ms so this would be two "ticks"
+            // Generally people don't type this fast
+            if ((DateTime.Now - _lastInputTime).TotalMilliseconds < 30)
+                return;
+
+            _lastInputTime = DateTime.Now;
+
             CharEntered?.Invoke(null, new CharEnteredEventArgs(e.Character));
         }
 
